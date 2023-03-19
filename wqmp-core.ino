b0d7cada56c394f3335cf307b1c253f928ff9dc2
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-18 18:14:14 -0600

    Init

---
+++ wqmp-core.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+
+#include <WiFi.h>
+// #include <WiFiClient.h>
+// #include <WiFiNINA.h>
+// #include <WiFiServer.h>
+// #include <WiFiSSLClient.h>
+// #include <WiFiStorage.h>
+// #include <WiFiUdp.h>
+
+WiFiClient client;
+char ssid[] = "";
+char pass[] = "";
+#define ENDL "\r\n"
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Serial is ready");
+  Serial.println();
+
+  Serial.print("SSID: '");
+  Serial.print(ssid);
+  Serial.println("'");
+  Serial.println("Attempting wifi connection");
+
+  int status = WiFi.begin(ssid, pass);
+  if ( status != WL_CONNECTED) {
+    Serial.println("Couldn't get a wifi connection");
+    Serial.println("Stopping!");
+    return;
+  } else {
+    Serial.println("Got wifi connection");
+  }
+
+  if(client.connect("ntfy.sh", 80)) {
+    Serial.println(
+      "POST /IKLyZfnXFjUrFXYq HTTP/1.1" ENDL
+      "Host: ntfy.sh" ENDL
+      "Title: CustomTitle" ENDL
+      ENDL
+      "It works"
+    );
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}

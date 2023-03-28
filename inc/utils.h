34f55d4a2a153b5a8c77912c0c57e30e3c04fca3
SteveBeeblebrox <41750582+SteveBeeblebrox@users.noreply.github.com>
2023-03-27 19:35:06 -0600

    Update utils.h

--- inc/utils.h
+++ inc/utils.h
@@ -6,4 +6,14 @@
 /// @brief Converts analog 0-1023 reading to 0.0-5.0V 
 double a2v(const int analog);
 
+/// @brief Takes the average of an array
+template<typename T>
+T average(const size_t size, const T array[]) {
+    T sum = T(0);
+    for(size_t i = 0; i < size; i++) {
+        sum += array[i];
+    }
+    return sum / size;
+}
+
 #endif

ea2a76f7d93d10e74636e7ca06f1b6915e433eba
kwilson032 <127444852+kwilson032@users.noreply.github.com>
2023-03-31 15:53:37 -0600

    Update

--- wqmp-core.ino
+++ wqmp-core.ino
@@ -3,7 +3,7 @@
 #include "headers/utils.hpp"
 #include "headers/network.hpp"
 #include "headers/io.h"
-
+#include "headers/thresholds.h"
 #include <SPI.h>
 #include <WiFi.h>
 
@@ -49,26 +49,65 @@ void setup() {
 }
 
 template<typename T, typename F>
-T take_reading(const size_t count, F sample, T (*statistic)(size_t, T[]) = (T(*)(size_t, T*)) median<T>, const int delay_ms = 20) {
+T take_reading(const size_t count, F sample, const int delay_ms = 20) {
   T values[count] = {};
   for(size_t i = 0; i < count; i++) {
     values[i] = sample();
     delay(delay_ms);
   }
-  return statistic(count, values);
+  return median(count, values);
 }
+
+bool send_alert(String Message = ""){
+  return network::send_http(
+    network::HttpRequest {
+      ALERT_HOST, ALERT_PORT, ALERT_ROUTE,
+      "POST", Message, "text/plain"
+    }
+  );
+}
+unsigned long last_report = 0, last_alert_reset = 0;
+bool sent_pH_alert = false, sent_TDS_alert = false, sent_TBD_alert = false, sent_fluoro_alert = false;
 
 /// @brief Do repeated work
 void loop() {
   if(hasCriticalError) return;
+  Serial.println(sent_pH_alert);
 
-  double median_pH = take_reading<double>(10, get_pH);
+  double median_pH = 9.0; //take_reading<double>(10, get_pH);
+
   double median_TDS = take_reading<double>(10, get_TDS);
   double median_TBD = take_reading<double>(10, get_TBD);
   double median_fluoro = take_reading<double>(10, get_fluoro);
   
   
+  if(median_pH>SAFE_PH_MAX && !sent_pH_alert) sent_pH_alert = send_alert("pH is too high!");
+  if(median_pH<SAFE_PH_MIN && !sent_pH_alert) sent_pH_alert = send_alert("pH is too low!");
+  if(median_TDS>SAFE_TDS_MAX && !sent_TDS_alert) sent_TDS_alert = send_alert("TDS is too high!");
+  if(median_TBD>SAFE_TBD_MAX && !sent_TBD_alert) sent_TBD_alert = send_alert("TBD is too high!");
+  if(median_fluoro>SAFE_FLUORO_MAX && !sent_fluoro_alert) sent_fluoro_alert = send_alert("Fluorescence is too high!");
+
+  unsigned long now = millis ();
+  if(now-last_report>= 1000*60*30||last_report==0){
   unsigned int flow = flowsensor::get_flow();
+    last_report = now;
+    Serial.print ("Report sent");
+      String data = String ("")+"ph="+median_pH+";tds="+median_TDS+";tbd="+median_TBD+";fluoro="+median_fluoro+";flow="+flow;
+      network::send_http(
+        network::HttpRequest {
+          ENDPOINT_HOST, ENDPOINT_PORT, ENDPOINT_ROUTE,
+          "POST", data, "text/plain"
+        }
+      );
+    if(now-last_alert_reset>=1000*60*60*12){
+      last_alert_reset = now;
+      sent_pH_alert = sent_TDS_alert = sent_TBD_alert = sent_fluoro_alert = false;
+      
+    }
+  }
+  
+  
+
 
 
   // if(pH < 5) {
